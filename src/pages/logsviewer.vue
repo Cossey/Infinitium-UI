1e42da89aa6a3191940b08993895ab65c185a0d6
Stewart Cossey <stewart.cossey@gmail.com>
2021-04-23 22:26:40 +1200

    Fix logs viewer navbar menu items and add download and delete options.

--- src/pages/logsviewer.vue
+++ src/pages/logsviewer.vue
@@ -1,18 +1,48 @@
 <template>
   <f7-page name="logsviewer" ptr @ptr:refresh="fetchData">
-    <f7-navbar :title="'Log ' + file">
+    <f7-navbar>
       <f7-nav-left>
         <f7-link
           icon-ios="f7:menu"
           icon-aurora="f7:menu"
           icon-md="material:menu"
-          text="Logs"
           panel-open="left"
-          link="/logs/"
-          view=".view-left"
         ></f7-link>
       </f7-nav-left>
+      <f7-nav-title sliding>Log {{ file }}</f7-nav-title>
+      <f7-nav-right>
+        <f7-link
+          @click="deleteLog"
+          icon-aurora="f7:trash"
+          icon-md="material:delete"
+          v-if="theme.ios != true"
+        ></f7-link>
+        <f7-link
+          @click="downloadLog"
+          icon-aurora="f7:download_circle"
+          icon-md="material:download"
+          v-if="theme.ios != true"
+        ></f7-link>
+        <f7-link
+          @click="f7router.back()"
+          icon-ios="f7:multiply"
+          icon-aurora="f7:multiply"
+          icon-md="material:close"
+        ></f7-link>
+      </f7-nav-right>
     </f7-navbar>
+    <f7-toolbar position="bottom" v-if="theme.ios">
+        <f7-link
+          @click="deleteLog"
+          icon-ios="f7:trash"
+        ></f7-link>
+        <f7-link
+          @click="downloadLog"
+          icon-ios="f7:download_circle"
+        ></f7-link>
+      <f7-link>Download</f7-link>
+      <f7-link>Delete</f7-link>
+    </f7-toolbar>
     <f7-block>
       <p class="wrap">{{ log }}</p>
     </f7-block>
@@ -25,7 +55,7 @@
 </style>
 <script>
 import api from "../js/api";
-import { f7, f7ready, theme } from "framework7-vue";
+import { f7, f7ready, theme, f7router } from "framework7-vue";
 import { ref } from "vue";
 
 export default {
@@ -34,10 +64,12 @@ export default {
 
     return {
       log,
+      theme
     };
   },
   props: {
     file: String,
+    f7router: Object,
   },
   mounted() {
     f7ready((f7) => {
@@ -46,7 +78,6 @@ export default {
   },
   methods: {
     fetchData: function (done) {
-      console.log(this.file);
       api
         .getLog(this.file)
         .then((fileData) => {
