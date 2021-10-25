072c1e5f80663fb295cd20f091ecf89ca8cfa521
Stewart Cossey <stewart.cossey@gmail.com>
2021-10-26 01:42:03 +1300

    Tweak 404 image.

--- src/pages/404.vue
+++ src/pages/404.vue
@@ -2,15 +2,19 @@
   <f7-page>
     <f7-navbar title="Not found" back-link="Back"></f7-navbar>
 
-    <f7-block strong class="text-align-center">
+    <f7-block class="text-align-center">
       <p>
-        <i class="f7-icons if-ios" style="font-size: 100pt;">exclamationmark_triangle</i>
-        <i class="material-icons if-not-ios" style="font-size: 100pt;">report_problem</i>
+        <i class="f7-icons if-ios" style="font-size: 100pt"
+          >exclamationmark_triangle</i
+        >
+        <i class="material-icons if-not-ios" style="font-size: 100pt"
+          >report_problem</i
+        >
       </p>
       <p>Page Not Found</p>
     </f7-block>
   </f7-page>
 </template>
 <script>
-  export default {};
+export default {};
 </script>
