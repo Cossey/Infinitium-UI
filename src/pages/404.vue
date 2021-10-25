3cd452cbbe7e9642783249e4c56b6ce8c0726e98
Stewart Cossey <stewart.cossey@gmail.com>
2021-10-25 21:05:13 +1300

    Update page 404

--- src/pages/404.vue
+++ src/pages/404.vue
@@ -1,8 +1,13 @@
 <template>
   <f7-page>
     <f7-navbar title="Not found" back-link="Back"></f7-navbar>
-    <f7-block strong>
-      <p>Requested content not found.</p>
+
+    <f7-block strong class="text-align-center">
+      <p>
+        <i class="f7-icons if-ios" style="font-size: 100pt;">exclamationmark_triangle</i>
+        <i class="material-icons if-not-ios" style="font-size: 100pt;">report_problem</i>
+      </p>
+      <p>Page Not Found</p>
     </f7-block>
   </f7-page>
 </template>
